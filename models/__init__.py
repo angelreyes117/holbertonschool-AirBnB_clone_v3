02fc0ef8603b42bbfd1f0a6fd7718272d053ef19
angelreyes117 <angelreyes.117@gmail.com>
2025-06-25 04:08:59 +0000

    airbnb clone v3

--- models/__init__.py
+++ models/__init__.py
@@ -5,13 +5,14 @@ initialize the models package
 
 from os import getenv
 
-
 storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
+    class_dictionary = DBStorage.class_dictionary
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
+    class_dictionary = FileStorage.class_dictionary
     storage = FileStorage()
 storage.reload()
