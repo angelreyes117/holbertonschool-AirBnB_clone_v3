15208be1eefe1513b8074b54c63b4511ce097255
angelreyes117 <angelreyes.117@gmail.com>
2025-09-25 23:30:49 +0000

    aribnb clone v3

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -20,6 +20,7 @@ import json
 import os
 import pep8
 import unittest
+
 DBStorage = db_storage.DBStorage
 classes = {"Amenity": Amenity, "City": City, "Place": Place,
            "Review": Review, "State": State, "User": User}
@@ -40,10 +41,11 @@ class TestDBStorageDocs(unittest.TestCase):
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_test_db_storage(self):
-        """Test tests/test_models/test_db_storage.py conforms to PEP8."""
+        """Test test_db_storage.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_models/test_engine/\
-test_db_storage.py'])
+        result = pep8s.check_files(
+            ['tests/test_models/test_engine/test_db_storage.py']
+        )
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
@@ -65,12 +67,15 @@ test_db_storage.py'])
         """Test for the presence of docstrings in DBStorage methods"""
         for func in self.dbs_f:
             self.assertIsNot(func[1].__doc__, None,
-                             "{:s} method needs a docstring".format(func[0]))
+                             "{:s} method needs a docstring"
+                             .format(func[0]))
             self.assertTrue(len(func[1].__doc__) >= 1,
-                            "{:s} method needs a docstring".format(func[0]))
+                            "{:s} method needs a docstring"
+                            .format(func[0]))
 
 
-@unittest.skipIf(type(models.storage) == FileStorage, "Testing FileStorage")
+@unittest.skipIf(isinstance(models.storage, FileStorage),
+                 "Testing FileStorage")
 class TestStateDBInstances(unittest.TestCase):
     """DBStorage State Tests"""
     def tearDownClass():
